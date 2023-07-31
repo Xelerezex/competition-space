2258bdbe97e235a4394db1dde940354ad225127d
Nikita Beletsky <nibeletsky@gmail.com>
2023-07-31 12:00:15 +0300

    231. Fully Done.

--- leetcode-tasks/231 Power Of Two/poweroftwo.cpp
+++ leetcode-tasks/231 Power Of Two/poweroftwo.cpp
@@ -1,12 +1,36 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <bitset>
+#include <numeric>
 #include <cmath>
 
 class Solution
 {
 public:
+    static bool isApproximatelyZero (double number,
+                                     int    rounding = 0,
+                                     double tolerance
+                                     = std::numeric_limits<double>::epsilon())
+    {
+        if (std::fabs(number) <= tolerance * std::pow(10, rounding))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    double logbase (double number, double base)
+    {
+        if (number == 0.0)
+        {
+            return 1.0;
+        }
+
+        return static_cast<double>(log(number))
+               / static_cast<double>(log(base));
+    }
+
     bool isPowerOfTwo (int n)
     {
         if (n <= 0)
@@ -18,43 +42,66 @@ public:
             return true;
         }
 
-        /*
-        TODO: Try
-        double logbase(double a, double base)
-        {
-        return log(a) / log(base);
-        }
-
-        int main()
-        {
-        double x = 65536;
-        double result;
-
-        result = logbase(x, 2);
-        printf("Log base 2 of %lf is %lf\n", x, result);
-        }
-        */
-        // double square{std::sqrt(n)};
+        double base = logbase(static_cast<double>(n), 2.0);
         double tail{0.0};
+        double integer{0.0};
 
-        tail = std::modf(square, &square);
+        tail = std::modf(base, &integer);
 
-        std::cout << square << " " << tail << std::endl;
-
-        if (tail != 0.0)
+        if (!isApproximatelyZero(tail, 5))
         {
             return false;
         }
 
-        long shiftedNumber{0 | (1 << static_cast<int>(square))};
-
-        std::cout << std::bitset<std::numeric_limits<long>::digits>(
-            shiftedNumber)
-                  << " == " << shiftedNumber << std::endl;
-
-        return n == shiftedNumber;
+        return true;
     }
 };
+
+/* *************************** BEST PRACTISE ******************************* */
+/* bool isPowerOfTwo (int n)                                                 */
+/* {                                                                         */
+/*     if (n <= 0)                                                           */
+/*         return false;                                                     */
+/*     return ((n & (n - 1)) == 0);                                          */
+/* }                                                                         */
+/* ************************************************************************* */
+
+/* ----------------------- TRY LOGTEST ------------------------------------- */
+TEST(TestingLogBase, Test0)
+{
+    const double input_data{0};
+    const double base{2};
+    const double expected{1};
+
+    EXPECT_DOUBLE_EQ(Solution{}.logbase(input_data, base), expected);
+}
+
+TEST(TestingLogBase, Test1)
+{
+    const double input_data{16};
+    const double base{2};
+    const double expected{4};
+
+    EXPECT_DOUBLE_EQ(Solution{}.logbase(input_data, base), expected);
+}
+
+TEST(TestingLogBase, Test2)
+{
+    const double input_data{65536};
+    const double base{2};
+    const double expected{16};
+
+    EXPECT_DOUBLE_EQ(Solution{}.logbase(input_data, base), expected);
+}
+
+TEST(TestingLogBase, Test3)
+{
+    const double input_data{60'000};
+    const double base{2};
+    const double expected{15.872674880270607};
+
+    EXPECT_DOUBLE_EQ(Solution{}.logbase(input_data, base), expected);
+}
 
 /* ----------------------- GENERAL TESTS FROM LEETCODE --------------------- */
 TEST(TestingPositiveInput, Input1)
@@ -68,7 +115,7 @@ TEST(TestingPositiveInput, Input1)
 TEST(TestingPositiveInput, Input2)
 {
     const int  input_data{2};
-    const bool expected{false};
+    const bool expected{true};
 
     EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
 }
@@ -77,6 +124,22 @@ TEST(TestingPositiveInput, Input16)
 {
     const int  input_data{16};
     const bool expected{true};
+
+    EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
+}
+
+TEST(TestingPositiveInput, Input17)
+{
+    const int  input_data{17};
+    const bool expected{false};
+
+    EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
+}
+
+TEST(TestingPositiveInput, Input2_30_minus_3)
+{
+    const int  input_data{static_cast<int>(std::pow(2, 30)) - 3};
+    const bool expected{false};
 
     EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
 }
@@ -92,6 +155,23 @@ TEST(TestingPositiveInput, Input2_30)
 TEST(TestingPositiveInput, Input2_31)
 {
     const int  input_data{static_cast<int>(std::pow(2, 31))};
+    const bool expected{false};
+
+    EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
+}
+
+TEST(TestingPositiveInput, InputHuge1)
+{
+    // const int  input_data{536'870'912};
+    const int  input_data{static_cast<int>(std::pow(2, 29))};
+    const bool expected{true};
+
+    EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
+}
+
+TEST(TestingPositiveInput, InputHuge2)
+{
+    const int  input_data{1048577};
     const bool expected{false};
 
     EXPECT_EQ(Solution{}.isPowerOfTwo(input_data), expected);
