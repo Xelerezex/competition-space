a96c06487bee5978c6be0fd4b017c3053612b17d
Xelerezex <nikitavalderman@gmail.com>
2025-03-02 11:43:41 +0300

    master: Added new 209 problem.

--- leetcode-tasks/209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ leetcode-tasks/209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -8,13 +8,33 @@ public:
 
     int minSubArrayLen(int target, std::vector<int>& nums)
     {
-        
+        int result = 0;
+        return result;
     }
 };
 
-TEST(SampleTest, Addition)
+TEST (SolutionTest, Example1)
 {
-    EXPECT_EQ(2 + 2, 4);
+    const int target = 7;
+    std::vector<int> nums = { 2, 3, 1, 2, 4, 3 };
+    const int expected = 2;
+    EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
+}
+
+TEST (SolutionTest, Example2)
+{
+    const int target = 4;
+    std::vector<int> nums = { 1, 4, 4 };
+    const int expected = 2;
+    EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
+}
+
+TEST (SolutionTest, Example3)
+{
+    const int target = 11;
+    std::vector<int> nums = { 1,1,1,1,1,1,1,1 };
+    const int expected = 0;
+    EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
 }
 
 int main(int argc, char **argv)
