6cf6a8b806b70728b25fbbf1d2c82f69e8e40a2e
Xelerezex <nikitavalderman@gmail.com>
2025-03-28 09:58:48 +0300

    master: Added solved 209 problem.

--- leetcode-tasks/209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ leetcode-tasks/209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,5 +1,6 @@
-#include <gtest/gtest.h>
+﻿#include <gtest/gtest.h>
 
+#include <limits>
 #include <vector>
 
 class Solution
@@ -8,10 +9,58 @@ public:
 
     int minSubArrayLen(int target, std::vector<int>& nums)
     {
-        int result = 0;
-        return result;
+        int minLength = std::numeric_limits<int>::max();
+
+        int currentSum = 0;
+
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right)
+        {
+            currentSum += nums[right];
+            if (target <= currentSum)
+            {
+                minLength = std::min(minLength, right - left + 1);
+            }
+            
+            while (currentSum > target && left <= right)
+            {
+                currentSum -= nums[left];
+                ++left;
+            
+                if (target <= currentSum)
+                {
+                    minLength = std::min(minLength, right - left + 1);
+                }
+            }
+        }
+
+        return std::numeric_limits<int>::max() == minLength ? 0 : minLength;
     }
 };
+
+TEST (SolutionTest, CornerCase1)
+{
+    const int target = 6;
+    std::vector<int> nums = { 10,2,3 };
+    const int expected = 1;
+    EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
+}
+
+TEST (SolutionTest, CornerCase2)
+{
+    const int target = 15;
+    std::vector<int> nums = { 1,2,3,4,5 };
+    const int expected = 5;
+    EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
+}
+
+TEST (SolutionTest, CornerCase3)
+{
+    const int target = 11;
+    std::vector<int> nums = { 1,2,3,4,5 };
+    const int expected = 3;
+    EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
+}
 
 TEST (SolutionTest, Example1)
 {
@@ -25,7 +74,7 @@ TEST (SolutionTest, Example2)
 {
     const int target = 4;
     std::vector<int> nums = { 1, 4, 4 };
-    const int expected = 2;
+    const int expected = 1;
     EXPECT_EQ(expected, Solution{}.minSubArrayLen(target, nums));
 }
 
