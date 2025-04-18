55886143e67891ff8cc80fd49b49318c22ec769f
Xelerezex <nikitavalderman@gmail.com>
2025-04-18 11:31:45 +0300

    master: Added solved 239 problem.

---
+++ leetcode-tasks/239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,108 @@
+﻿#include <algorithm>
+#include <gtest/gtest.h>
+#include <iterator>
+#include <set>
+
+class Solution
+{
+
+public:
+    
+    std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k)
+    {
+        if (nums.size() < k)
+        {
+            int value = *std::max_element(
+                std::begin(nums), 
+                std::end(nums)
+            );
+            return { value };
+        }
+
+        std::vector<int> result;
+        std::multiset<int> window;
+
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right)
+        {
+            const auto newValue = nums[right];
+            window.emplace(newValue);
+
+            const auto currentWindowLength = (right - left) + 1;
+
+            if (currentWindowLength == k)
+            {
+                const auto& value = *std::crbegin(window);
+                result.push_back(value);
+
+                const auto oldValue = nums[left];
+                const auto toRemoveIter = window.lower_bound(oldValue);
+                window.erase(toRemoveIter);
+                ++left;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+TEST (SolutionTest, CornerCase1)
+{
+    std::vector<int> nums = { -7,-8,7,5,7,1,6,0 }; 
+    const int k = 4;
+    const auto result = Solution{}.maxSlidingWindow(nums, k);
+    const auto& expected = std::vector<int>{ 7,7,7,7,7 };
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, CornerCase2)
+{
+    std::vector<int> nums = {3 }; 
+    const int k = 3;
+    const auto result = Solution{}.maxSlidingWindow(nums, k);
+    const auto& expected = std::vector<int>{3 };
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, CornerCase3)
+{
+    std::vector<int> nums = {3, 1, -1 }; 
+    const int k = 3;
+    const auto result = Solution{}.maxSlidingWindow(nums, k);
+    const auto& expected = std::vector<int>{3 };
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example1)
+{
+    std::vector<int> nums = {1,3,-1,-3,5,3,6,7}; 
+    const int k = 3;
+    const auto result = Solution{}.maxSlidingWindow(nums, k);
+    const auto& expected = std::vector<int>{3,3,5,5,6,7};
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::vector<int> nums = {1}; 
+    const int k = 1;
+    const auto result = Solution{}.maxSlidingWindow(nums, k);
+    const auto& expected = std::vector<int>{1};
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::vector<int> nums = {1,2,1,0,4,2,6}; 
+    const int k = 3;
+    const auto result = Solution{}.maxSlidingWindow(nums, k);
+    const auto& expected = std::vector<int>{2,2,4,4,6};
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/239-sliding-window-maximum/README.md
@@ -0,0 +1,31 @@
+﻿[239. Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum/)
+
+You are given an array of integers `nums`, there is a sliding window of size `k` which is moving from the very left of the array to the very right. You can only see the `k` numbers in the window. Each time the sliding window moves right by one position.
+
+Return the _max sliding window_.
+
+__Example 1:__
+
+    Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+    Output: [3,3,5,5,6,7]
+    Explanation: 
+    Window position                Max
+    ---------------               -----
+    [1  3  -1] -3  5  3  6  7       3
+    1  [3  -1  -3] 5  3  6  7       3
+    1   3 [-1  -3  5] 3  6  7       5
+    1   3  -1 [-3  5  3] 6  7       5
+    1   3  -1  -3 [5  3  6] 7       6
+    1   3  -1  -3  5 [3  6  7]      7
+
+__Example 2:__
+
+    Input: nums = [1], k = 1
+    Output: [1]
+
+__Constraints:__
+
+- $1 \leq$ `nums.length` $\leq 10^5$
+- $-10^4 \leq$ `nums[i]` $\leq 10^4$
+- $1 \leq$ `k` $\leq$ `nums.length`
+
