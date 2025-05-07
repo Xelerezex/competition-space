2a77cb6eba51a4217c6447bdfe769daf488af294
Xelerezex <nikitavalderman@gmail.com>
2025-05-07 12:32:21 +0300

    master: Added solved 74 problem.

---
+++ leetcode-tasks/74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,141 @@
+﻿#include <gtest/gtest.h>
+
+class Solution
+{
+public:
+    
+    bool isTargetInArray(
+        const std::vector<int>& data,
+        int target
+    )
+    {
+        std::size_t leftIndex = 0;
+        std::size_t rightIndex = data.size() - 1;
+
+        while (leftIndex <= rightIndex
+            && (leftIndex < data.size() && rightIndex < data.size())
+        )
+        {
+            std::size_t middle = leftIndex + ((rightIndex - leftIndex) / 2);
+
+            if (target == data[middle])
+            {
+                return true;
+            }
+            else if (target < data[middle])
+            {
+                rightIndex = middle - 1;
+            }
+            else if (target > data[middle])
+            {
+                leftIndex = middle + 1;
+            }
+        }
+
+        return false;
+    }
+
+    bool searchMatrix(
+        std::vector<std::vector<int>>& matrix, 
+        int target
+    )
+    {
+
+        std::size_t upIndex = 0;
+        std::size_t downIndex = matrix.size() - 1;
+
+        while (upIndex <= downIndex
+            && (upIndex < matrix.size() && downIndex < matrix.size())
+        )
+        {
+            std::size_t middle = upIndex + ((downIndex - upIndex) / 2);
+            const auto& middleData = matrix.at(middle);
+            const int dataLeftValue = *std::cbegin(middleData);
+            const int dataRightValue = *std::crbegin(middleData);
+
+            if (isTargetInArray(middleData, target))
+            {
+                return true;
+            }
+            else if (dataRightValue < target)
+            {
+                upIndex = middle + 1;
+            }
+            else if (dataRightValue > target)
+            {
+                downIndex = middle - 1;
+            }
+        }
+
+        return false;
+    }
+};
+
+TEST (SolutionTest, CornerCase1)
+{
+    std::vector<std::vector<int>> matrix = {
+        { 3 }
+    };
+    int target = 3;
+    const auto result = Solution{}.searchMatrix(matrix, target);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::vector<std::vector<int>> matrix = {
+        { 1,2,4,8 },
+        { 10,11,12,13 },
+        { 14,20,30,40 }
+    };
+    int target = 10;
+    const auto result = Solution{}.searchMatrix(matrix, target);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example1)
+{
+    std::vector<std::vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+    int target = 3;
+    const auto result = Solution{}.searchMatrix(matrix, target);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::vector<std::vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+    int target = 13;
+    const auto result = Solution{}.searchMatrix(matrix, target);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example4)
+{
+    std::vector<std::vector<int>> matrix = {
+        { 1,2,4,8 },
+        { 10,11,12,13 },
+        { 14,20,30,40 }
+    };
+    int target = 15;
+    const auto result = Solution{}.searchMatrix(matrix, target);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/74-search-a-2d-matrix/README.md
@@ -0,0 +1,29 @@
+﻿[74. Search a 2D Matrix](https://leetcode.com/problems/search-a-2d-matrix/description/)
+
+You are given an `m x n` integer matrix matrix with the following two properties:
+
+- Each row is sorted in non-decreasing order.
+- The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer `target`, return `true` if `target` is in `matrix` or `false` otherwise.
+
+You must write a solution in `O(log(m * n))` time complexity.
+
+__Example 1:__
+    ![image](./../../images/74-task-matrix-1.jpg)
+
+    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+    Output: true
+
+__Example 2:__
+    ![image](./../../images/74-task-matrix-2.jpg)
+    
+    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+    Output: false
+
+__Constraints:__
+
+- `m == matrix.length`
+- `n == matrix[i].length`
+- $1 \leq$ `m, n` $\leq 100$
+- $-10^4 \leq$ `matrix[i][j], target` $\leq 10^4$
