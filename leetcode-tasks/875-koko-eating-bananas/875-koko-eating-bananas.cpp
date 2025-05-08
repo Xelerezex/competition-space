3de7aa17f78356cd4677fabb5ed453d7049a7e68
Xelerezex <nikitavalderman@gmail.com>
2025-05-08 14:02:48 +0300

    master: Added solved 875 problem.

---
+++ leetcode-tasks/875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,109 @@
+﻿#include <algorithm>
+#include <gtest/gtest.h>
+
+class Solution
+{
+public:
+
+    bool checkData(const std::vector<int>& piles, std::size_t currentRatio, int h)
+    {
+        std::size_t summ = 0;
+        for (const auto& pile : piles)
+        {
+            const double ratio = static_cast<double>(pile) / static_cast<double>(currentRatio);
+            const auto ratioPerPile = static_cast<std::size_t>(std::ceil(ratio));
+            summ += ratioPerPile;
+        }
+        return summ <= h;
+    };
+
+    int minEatingSpeed(std::vector<int>& piles, int h)
+    {
+        const int maxPile = *std::max_element(std::cbegin(piles), std::cend(piles));
+
+        std::size_t left = 1;
+        std::size_t right = maxPile;
+
+        std::size_t result = maxPile;
+
+        while (left <= right 
+            && (left <= maxPile && right <= maxPile)
+        )
+        {
+            std::size_t middle = left + ((right - left) / 2);
+
+            if (checkData(piles, middle, h))
+            {
+                right = middle - 1;
+
+                result = std::min(result, middle);
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return static_cast<int>(result);
+    }
+};
+  
+TEST (SolutionTest, CornerCase1)
+{
+    std::vector<int> piles = { 805306368, 805306368, 805306368 };
+    const int h = 1000000000;
+    const auto result = Solution{}.minEatingSpeed(piles, h);
+    const auto& expected = 3;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example1)
+{
+    std::vector<int> piles = { 3,6,7,11 };
+    const int h = 8;
+    const auto result = Solution{}.minEatingSpeed(piles, h);
+    const auto& expected = 4;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::vector<int> piles = { 30,11,23,4,20 };
+    const int h = 5;
+    const auto result = Solution{}.minEatingSpeed(piles, h);
+    const auto& expected = 30;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::vector<int> piles = { 1,4,3,2 };
+    const int h = 9;
+    const auto result = Solution{}.minEatingSpeed(piles, h);
+    const auto& expected = 2;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example4)
+{
+    std::vector<int> piles = { 30,11,23,4,20 };
+    const int h = 6;
+    const auto result = Solution{}.minEatingSpeed(piles, h);
+    const auto& expected = 23;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example5)
+{
+    std::vector<int> piles = { 25,10,23,4 };
+    const int h = 4;
+    const auto result = Solution{}.minEatingSpeed(piles, h);
+    const auto& expected = 25;
+    EXPECT_EQ(result, expected);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/875-koko-eating-bananas/README.md
@@ -0,0 +1,27 @@
+﻿[](https://neetcode.io/problems/eating-bananas)
+
+You are given an integer array `piles` where `piles[i]` is the number of bananas in the `ith` pile. You are also given an integer h, which represents the number of hours you have to eat all the bananas.
+
+You may decide your bananas-per-hour eating rate of `k`. Each hour, you may choose a pile of bananas and eats `k` bananas from that pile. If the pile has less than `k` bananas, you may finish eating the pile but you can not eat from another pile in the same hour.
+
+Return the minimum integer `k` such that you can eat all the bananas within `h` hours.
+
+__Example 1:__
+
+    Input: piles = [1,4,3,2], h = 9
+
+    Output: 2
+
+_Explanation:_ With an eating rate of 2, you can eat the bananas in 6 hours. With an eating rate of 1, you would need 10 hours to eat all the bananas (which exceeds h=9), thus the minimum eating rate is 2.
+
+__Example 2:__
+
+    Input: piles = [25,10,23,4], h = 4
+
+    Output: 25
+
+__Constraints:__
+
+- $1 \leq$ `piles.length` $\leq 1,000$
+- `piles.length` $\leq$ `h` $\leq 1,000,000$
+- $1 \leq$ `piles[i]` $\leq 1,000,000,000$
