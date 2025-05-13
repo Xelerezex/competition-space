3a10f33f6d0fcf1b6e2d0d39e4629e148bb27ed1
Xelerezex <nikitavalderman@gmail.com>
2025-05-13 09:26:45 +0300

    master: Added solved 206 problem.

---
+++ leetcode-tasks/206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -0,0 +1,113 @@
+﻿#include <gtest/gtest.h>
+#include <memory>
+#include <vector>
+
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+
+    ListNode* reverseList(ListNode* pHead)
+    {
+        if (pHead == nullptr)
+        {
+            return nullptr;
+        }
+
+        ListNode* pPreviousNode = nullptr; 
+        ListNode* pCurrentNode = pHead; 
+
+        while (pCurrentNode != nullptr)
+        {
+            // Save pointer to next
+            ListNode* pNextNode = pCurrentNode->next;
+
+            // Switch pointers
+            pCurrentNode->next = pPreviousNode;
+            pPreviousNode = pCurrentNode;
+
+            // Next iteration
+            pCurrentNode = pNextNode;
+        }
+
+        return pPreviousNode;
+    };
+
+};
+
+TEST (SolutionTest, Example1)
+{
+    auto node4 = std::make_unique<ListNode>(3, nullptr);
+    auto node3 = std::make_unique<ListNode>(2, node4.get());
+    auto node2 = std::make_unique<ListNode>(1, node3.get());
+    auto node1 = std::make_unique<ListNode>(0, node2.get());
+    std::vector<ListNode*> reverseDirection;
+    reverseDirection.push_back(node4.get());
+    reverseDirection.push_back(node3.get());
+    reverseDirection.push_back(node2.get());
+    reverseDirection.push_back(node1.get());
+
+    auto pResult = Solution{}.reverseList(node1.get());
+    int indexCounter = 0;
+
+    auto pCurrentNode = pResult;
+    while (nullptr != pCurrentNode)
+    {
+        EXPECT_EQ(reverseDirection[indexCounter++], pCurrentNode);
+
+        pCurrentNode = pCurrentNode->next;
+    }
+}
+
+TEST (SolutionTest, Example2)
+{
+    auto node2 = std::make_unique<ListNode>(2, nullptr);
+    auto node1 = std::make_unique<ListNode>(1, node2.get());
+    std::vector<ListNode*> reverseDirection;
+    reverseDirection.push_back(node2.get());
+    reverseDirection.push_back(node1.get());
+
+    auto pResult = Solution{}.reverseList(node1.get());
+    int indexCounter = 0;
+
+    auto pCurrentNode = pResult;
+    while (nullptr != pCurrentNode)
+    {
+        EXPECT_EQ(reverseDirection[indexCounter++], pCurrentNode);
+
+        pCurrentNode = pCurrentNode->next;
+    }
+}
+
+TEST (SolutionTest, Example3)
+{
+    auto node1 = std::make_unique<ListNode>(1, nullptr);
+    std::vector<ListNode*> reverseDirection;
+    reverseDirection.push_back(node1.get());
+
+    auto pResult = Solution{}.reverseList(node1.get());
+    int indexCounter = 0;
+
+    auto pCurrentNode = pResult;
+    while (nullptr != pCurrentNode)
+    {
+        EXPECT_EQ(reverseDirection[indexCounter++], pCurrentNode);
+
+        pCurrentNode = pCurrentNode->next;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/206-reverse-linked-list/README.md
@@ -0,0 +1,20 @@
+﻿[Reverse Linked List](https://neetcode.io/problems/reverse-a-linked-list)
+
+Given the beginning of a singly linked list `head`, reverse the list, and return the new beginning of the list.
+
+__Example 1:__
+
+    Input: head = [0,1,2,3]
+
+    Output: [3,2,1,0]
+
+__Example 2:__
+
+    Input: head = []
+
+    Output: []
+
+__Constraints:__
+
+- $0 \leq$ `The length of the list` $\leq 1000$.
+- $-1000 \leq$ `Node.val` $\leq 1000$
