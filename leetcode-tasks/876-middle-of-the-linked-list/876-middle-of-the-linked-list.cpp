246b04090154221799ff31dab0ec016df80c67ef
Xelerezex <nikitavalderman@gmail.com>
2025-05-16 10:43:40 +0300

    master: Added solved 876 problem.

---
+++ leetcode-tasks/876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -0,0 +1,127 @@
+﻿#include <cmath>
+#include <gtest/gtest.h>
+
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/*
+// NOTE: My solution
+class Solution
+{
+public:
+    ListNode* middleNode(ListNode* head)
+    {
+        int elementCounter = 0;
+        for (auto* pCurrentNode = head; nullptr != pCurrentNode; pCurrentNode = pCurrentNode->next)
+        {
+           elementCounter++;
+        }
+
+        const double delimiter = 2;
+        int middleElement = (elementCounter % 2 == 0) 
+            ? static_cast<int>(static_cast<double>(elementCounter) / delimiter) + 1
+            : static_cast<int>(std::ceil(static_cast<double>(elementCounter) / delimiter));
+
+        elementCounter = 0;
+        for (auto* pCurrentNode = head; nullptr != pCurrentNode; pCurrentNode = pCurrentNode->next)
+        {
+            elementCounter++;
+            if (middleElement == elementCounter)
+            {
+                return pCurrentNode;
+            }
+        }
+
+        return nullptr;
+    }
+};
+*/
+
+// Faster solution
+class Solution
+{
+public:
+    ListNode* middleNode(ListNode* head)
+    {
+        ListNode* pSlow = head;
+        ListNode* pFast = head;
+        
+        while (nullptr != pFast && nullptr != pFast->next)
+        {
+            pSlow = pSlow->next;
+            pFast = pFast->next->next;
+        }
+
+        return pSlow;
+    }
+};
+
+TEST (SolutionTest, Example1)
+{
+    auto node7 = std::make_unique<ListNode>(6, nullptr);
+    auto node6 = std::make_unique<ListNode>(5, node7.get());
+    auto node5 = std::make_unique<ListNode>(4, node6.get());
+    auto node4 = std::make_unique<ListNode>(3, node5.get()); // <-
+    auto node3 = std::make_unique<ListNode>(6, node4.get());
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(1, node2.get());
+
+    auto pResult = Solution{}.middleNode(node1.get());
+    EXPECT_EQ(3, pResult->val);
+}
+
+TEST (SolutionTest, Example2)
+{
+    auto node5 = std::make_unique<ListNode>(5, nullptr);
+    auto node4 = std::make_unique<ListNode>(4, node5.get());
+    auto node3 = std::make_unique<ListNode>(3, node4.get()); // <-
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(1, node2.get());
+
+    auto pResult = Solution{}.middleNode(node1.get());
+    EXPECT_EQ(3, pResult->val);
+}
+
+TEST (SolutionTest, Example3)
+{
+    auto node6 = std::make_unique<ListNode>(6, nullptr);
+    auto node5 = std::make_unique<ListNode>(5, node6.get());
+    auto node4 = std::make_unique<ListNode>(4, node5.get()); // <-
+    auto node3 = std::make_unique<ListNode>(3, node4.get());
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(1, node2.get());
+
+    auto pResult = Solution{}.middleNode(node1.get());
+    EXPECT_EQ(4, pResult->val);
+}
+
+TEST (SolutionTest, Example4)
+{
+    auto node1 = std::make_unique<ListNode>(9, nullptr); // <-
+
+    auto pResult = Solution{}.middleNode(node1.get());
+    EXPECT_EQ(9, pResult->val);
+}
+
+TEST (SolutionTest, Example5)
+{
+    auto node2 = std::make_unique<ListNode>(20, nullptr); // <-
+    auto node1 = std::make_unique<ListNode>(10, node2.get());
+
+    auto pResult = Solution{}.middleNode(node1.get());
+    EXPECT_EQ(20, pResult->val);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/876-middle-of-the-linked-list/README.md
@@ -0,0 +1,27 @@
+﻿[876. Middle of the Linked List](https://leetcode.com/problems/middle-of-the-linked-list/)
+
+Given the `head` of a singly linked list, return _the middle node_ of the linked list.
+
+If there are two middle nodes, return __the second middle__ node.
+
+__Example 1:__
+
+    ![images](./../../images/876-middle-of-the-linked-list-1.jpg)
+
+    Input: head = [1,2,3,4,5]
+    Output: [3,4,5]
+    Explanation: The middle node of the list is node 3.
+
+__Example 2:__
+
+    ![images](./../../images/876-middle-of-the-linked-list-2.jpg)
+
+    Input: head = [1,2,3,4,5,6]
+    Output: [4,5,6]
+    Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+__Constraints:__
+
+-    The number of nodes in the list is in the range `[1, 100]`.
+-    $1 \leq $ `Node.val` $ \leq 100$
+
