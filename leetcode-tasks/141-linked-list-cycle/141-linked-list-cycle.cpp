fdb2d4d26f50543f10465608a96fd8ef113d38b2
Xelerezex <nikitavalderman@gmail.com>
2025-05-19 09:51:44 +0300

    master: Added solved 141 problem.

---
+++ leetcode-tasks/141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -0,0 +1,86 @@
+﻿#include <gtest/gtest.h>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode* head)
+    {
+        ListNode* pSlow = head;
+        ListNode* pFast = head;
+        
+        while (nullptr != pFast && nullptr != pFast->next)
+        {
+            pSlow = pSlow->next;
+            pFast = pFast->next->next;
+
+            // If pointers intersects - there is a loop
+            if (pSlow == pFast)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+TEST (SolutionTest, Example1)
+{
+    auto node4 = std::make_unique<ListNode>(-4, nullptr);
+    auto node3 = std::make_unique<ListNode>(0, node4.get());
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(3, node2.get());
+    node4->next = node2.get();
+
+    const auto result = Solution{}.hasCycle(node1.get());
+    const auto expected = true;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example2)
+{
+    auto node4 = std::make_unique<ListNode>(-4, nullptr);
+    auto node3 = std::make_unique<ListNode>(0, node4.get());
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(3, node2.get());
+
+    const auto result = Solution{}.hasCycle(node1.get());
+    const auto expected = false;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example3)
+{
+    auto node2 = std::make_unique<ListNode>(2, nullptr);
+    auto node1 = std::make_unique<ListNode>(1, node2.get());
+    node2->next = node1.get();
+
+    const auto result = Solution{}.hasCycle(node1.get());
+    const auto expected = true;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example4)
+{
+    auto node1 = std::make_unique<ListNode>(1, nullptr);
+    
+    const auto result = Solution{}.hasCycle(node1.get());
+    const auto expected = false;
+    EXPECT_EQ(result, expected);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/141-linked-list-cycle/README.md
@@ -0,0 +1,39 @@
+﻿[141. Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/)
+
+Given `head`, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the `next` pointer. Internally, `pos` is used to denote the index of the node that tail's `next` pointer is connected to. __Note that__ `pos` __is not passed as a parameter__.
+
+Return `true` if there is a cycle in the linked list. Otherwise, return `false`.
+
+__Example 1:__
+
+![image](./../../images/141-linked-list-cycle-1.png)
+
+    Input: head = [3,2,0,-4], pos = 1
+    Output: true
+    Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+__Example 2:__
+
+![image](./../../images/141-linked-list-cycle-2.png)
+
+    Input: head = [1,2], pos = 0
+    Output: true
+    Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+__Example 3:__
+
+![image](./../../images/141-linked-list-cycle-3.png)
+
+    Input: head = [1], pos = -1
+    Output: false
+    Explanation: There is no cycle in the linked list.
+
+__Constraints:__
+
+- The number of the nodes in the list is in the range [0, 104].
+- $-10^5 \leq $Node.val$ \leq 10^5$
+- `pos` is `-1` or a _valid index_ in the linked-list.
+
+__Follow up__: Can you solve it using $O(1)$ (i.e. constant) memory?
