fcc151ee054121c76681ba888a717c0321e0d946
Xelerezex <nikitavalderman@gmail.com>
2025-05-19 10:16:21 +0300

    master: Added solved 142 problem.

---
+++ leetcode-tasks/142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,98 @@
+﻿#include <gtest/gtest.h>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+
+    ListNode* detectCycle(ListNode* head)
+    {
+        ListNode* pSlow = head;
+        ListNode* pFast = head;
+        ListNode* pIntersection = nullptr;
+
+        while (nullptr != pFast && nullptr != pFast->next)
+        {
+            pSlow = pSlow->next;
+            pFast = pFast->next->next;
+
+            if (pSlow == pFast)
+            {
+                pIntersection = pSlow;
+                break;
+            }
+        }
+
+        // From head and intersection we try to find start of the loop
+        while (nullptr != pIntersection 
+            && head != pIntersection
+        )
+        {
+            pIntersection = pIntersection->next;
+            head = head->next;
+        }
+
+        return pIntersection;
+    }
+
+};
+
+TEST (SolutionTest, Example1)
+{
+    auto node4 = std::make_unique<ListNode>(-4, nullptr);
+    auto node3 = std::make_unique<ListNode>(0, node4.get());
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(3, node2.get());
+    node4->next = node2.get();
+
+    const auto result = Solution{}.detectCycle(node1.get());
+    const auto expected = node2.get();
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example2)
+{
+    auto node4 = std::make_unique<ListNode>(-4, nullptr);
+    auto node3 = std::make_unique<ListNode>(0, node4.get());
+    auto node2 = std::make_unique<ListNode>(2, node3.get());
+    auto node1 = std::make_unique<ListNode>(3, node2.get());
+
+    const auto result = Solution{}.detectCycle(node1.get());
+    const auto expected = nullptr;
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example3)
+{
+    auto node2 = std::make_unique<ListNode>(2, nullptr);
+    auto node1 = std::make_unique<ListNode>(1, node2.get());
+    node2->next = node1.get();
+
+    const auto result = Solution{}.detectCycle(node1.get());
+    const auto expected = node1.get();
+    EXPECT_EQ(result, expected);
+}
+
+TEST (SolutionTest, Example4)
+{
+    auto node1 = std::make_unique<ListNode>(1, nullptr);
+    
+    const auto result = Solution{}.detectCycle(node1.get());
+    const auto expected = nullptr;
+    EXPECT_EQ(result, expected);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/142-linked-list-cycle-ii/README.md
@@ -0,0 +1,39 @@
+﻿[142. Linked List Cycle II](https://leetcode.com/problems/linked-list-cycle-ii/)
+
+Given the `head` of a linked list, return the node where the cycle begins. If there is no cycle, return `null`.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, `pos` is used to denote the index of the node that tail's `next` pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that `pos` is not passed as a parameter.
+
+Do not modify the linked list.
+
+__Example 1:__
+
+![image](./../../images/142-linked-list-cycle-ii-1.png)
+
+    Input: head = [3,2,0,-4], pos = 1
+    Output: tail connects to node index 1
+    Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+__Example 2:__
+
+![image](./../../images/142-linked-list-cycle-ii-2.png)
+
+    Input: head = [1,2], pos = 0
+    Output: tail connects to node index 0
+    Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+__Example 3:__
+
+![image](./../../images/142-linked-list-cycle-ii-3.png)
+
+    Input: head = [1], pos = -1
+    Output: no cycle
+    Explanation: There is no cycle in the linked list.
+
+__Constraints:__
+
+- The number of the nodes in the list is in the range [0, $10^4$].
+- $-10^5 \leq $Node.val$ \leq 10^5$
+- `pos` is `-1` or a _valid index_ in the linked-list.
+
+__Follow up:__ Can you solve it using $O(1)$ (i.e. constant) memory?
