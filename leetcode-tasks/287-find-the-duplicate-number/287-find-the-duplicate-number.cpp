453d6293c45ae2215d0122269e2a90d85d08d2d9
Xelerezex <nikitavalderman@gmail.com>
2025-05-20 08:47:26 +0300

    master: Added solved 287 problem.

---
+++ leetcode-tasks/287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,110 @@
+﻿#include <gtest/gtest.h>
+
+// NOTE: My first solution. And it is quiet efficient
+class Solution
+{
+
+public:
+
+    int findDuplicate(std::vector<int>& nums)
+    {
+        int result = 0;
+
+        for (std::size_t index = 0; index < nums.size(); ++index)
+        {
+            const int indexByValue = std::abs(nums[index]) - 1;
+            if (nums[indexByValue] <= 0)
+            {
+                result = std::abs(nums[index]);
+                break;
+            }
+
+            nums[indexByValue] *= -1;
+        }
+
+        return result;
+    }
+};
+
+
+// NOTE: Second solution with Floyds Tortoise & Hare algorithm
+//       It's cool, but it slower than upper solution
+/*
+class Solution
+{
+
+public:
+
+    int findDuplicate(std::vector<int>& nums)
+    {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find a loop
+        while (true)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast)
+            {
+                break;
+            }
+        }
+
+        // Find start of loop
+        int slow2 = nums[0];
+        while (slow2 != slow)
+        {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;
+    }
+};
+*/
+TEST (SolutionTest, CornerCase1)
+{
+    std::vector<int> nums{ 1, 3, 4, 2, 2 };
+    const auto result = Solution{}.findDuplicate(nums);
+    const auto& expected = 2;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example1)
+{
+    std::vector<int> nums{ 1, 2, 3, 2, 2 };
+    const auto result = Solution{}.findDuplicate(nums);
+    const auto& expected = 2;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::vector<int> nums{ 1, 2, 3, 4, 4 };
+    const auto result = Solution{}.findDuplicate(nums);
+    const auto& expected = 4;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::vector<int> nums{ 3, 1, 3, 4, 2 };
+    const auto result = Solution{}.findDuplicate(nums);
+    const auto& expected = 3;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example4)
+{
+    std::vector<int> nums{ 3, 3, 3, 3, 3 };
+    const auto result = Solution{}.findDuplicate(nums);
+    const auto& expected = 3;
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/287-find-the-duplicate-number/README.md
@@ -0,0 +1,34 @@
+﻿[287. Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/)
+
+Given an array of integers `nums` containing `n + 1` integers where each integer is in the range `[1, n]` inclusive.
+
+There is only one repeated number in `nums`, return _this repeated number_.
+
+You must solve the problem without modifying the array `nums` and using only constant extra space.
+
+__Example 1:__
+
+    Input: nums = [1,3,4,2,2]
+    Output: 2
+
+__Example 2:__
+
+    Input: nums = [3,1,3,4,2]
+    Output: 3
+
+__Example 3:__
+
+    Input: nums = [3,3,3,3,3]
+    Output: 3
+
+__Constraints:__
+
+- $1 \leq $`n`$ \leq 105$
+- `nums.length == n + 1`
+- $1 \leq $`nums[i]`$ \leq n$
+- All the integers in `nums` appear only __once__ except for __precisely one integer__ which appears two or more times.
+
+__Follow up:__
+
+- How can we prove that at least one duplicate number must exist in `nums`?
+- Can you solve the problem in linear runtime complexity?
