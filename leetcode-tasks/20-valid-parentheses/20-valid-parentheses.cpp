95b7aff804a0ab4e363a24368f6a9c78068aa896
Xelerezex <nikitavalderman@gmail.com>
2025-05-23 10:31:31 +0300

    master: Added solved 20 problem.

---
+++ leetcode-tasks/20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,174 @@
+﻿#include <gtest/gtest.h>
+#include <stack>
+
+class Solution
+{
+public:
+   
+    bool isLeftBracket(char bracket)
+    {
+        return '[' == bracket
+            || '(' == bracket
+            || '{' == bracket;
+    }
+
+    bool isRightBracket(char bracket)
+    {
+        return ']' == bracket
+            || ')' == bracket
+            || '}' == bracket;
+    }
+
+    char corespondingLeftBracket(char bracket)
+    {
+        char result = 0;
+        switch (bracket) 
+        {
+            case ']':
+            {
+                result = '[';
+            } break;
+            case ')':
+            {
+                result = '(';
+            } break;
+            case '}':
+            {
+                result = '{';
+            } break;
+            default:
+            {
+            } break;
+        }
+        return result;
+    }
+
+    bool isValid(std::string s)
+    {
+        std::stack<int> leftBrackets; 
+
+        for (const auto bracket : s)
+        {
+            if (isLeftBracket(bracket))
+            {
+                leftBrackets.push(bracket);
+            }
+            else if (isRightBracket(bracket))
+            {
+                const char currentLeft = corespondingLeftBracket(bracket);
+                if (leftBrackets.empty())
+                {
+                    return false;
+                }
+                if (currentLeft != leftBrackets.top())
+                {
+                    return false;
+                }
+                leftBrackets.pop();
+            }
+        }
+        return leftBrackets.empty();
+    }
+};
+
+TEST (SolutionTest, CornerCase0)
+{
+    std::string s = "]";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, CornerCase1)
+{
+    std::string s = "(";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, CornerCase2)
+{
+    std::string s = "((";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, CornerCase3)
+{
+    std::string s = "(()";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example1)
+{
+    std::string s = "()";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::string s = "()[]{}";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::string s = "(]";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example4)
+{
+    std::string s = "([])";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example5)
+{
+    std::string s = "[][]{}";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example6)
+{
+    std::string s = "([{}])";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example7)
+{
+    std::string s = "[(])";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = false;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example8)
+{
+    std::string s = "([]{})[]";
+    const auto result = Solution{}.isValid(s);
+    const auto& expected = true;
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/20-valid-parentheses/README.md
@@ -0,0 +1,34 @@
+﻿[20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)
+
+Given a string `s` containing just the characters `'('`, `')'`, `'{'`, `'}'`, `'['` and `']'`, determine if the input string is valid.
+
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+3. Every close bracket has a corresponding open bracket of the same type.
+
+__Example 1:__
+
+    Input: s = "()"
+    Output: true
+
+__Example 2:__
+
+    Input: s = "()[]{}"
+    Output: true
+
+__Example 3:__
+
+    Input: s = "(]"
+    Output: false
+
+__Example 4:__
+
+    Input: s = "([])"
+    Output: true
+
+__Constraints:__
+
+- $1 \leq $ `s.length` $ \leq 10^4$
+- `s` consists of parentheses only `'()[]{}'`.
