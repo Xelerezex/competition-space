10676061f723795a1441ff5424d666a8747a2186
Xelerezex <nikitavalderman@gmail.com>
2025-05-28 08:16:17 +0300

    master: Added solved 22 problem.

---
+++ leetcode-tasks/22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,54 @@
+﻿#include <gtest/gtest.h>
+
+class Solution
+{
+public:
+
+    std::vector<std::string> generateParenthesis(int n)
+    {
+        std::vector<std::vector<std::string>> result(n + 1);
+        result[0] = {""};
+
+        for (int numberIndex = 0; numberIndex <= n; ++numberIndex)
+        {
+            for (int index = 0; index < numberIndex; ++index)
+            {
+                for (const std::string& left : result[index])
+                {
+                    for (const std::string& right : result[numberIndex - index - 1])
+                    {
+                        result[numberIndex].push_back("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+
+        return result[n];
+    }
+};
+
+TEST (SolutionTest, Example1)
+{
+    const int n = 3;
+    const auto result = Solution{}.generateParenthesis(n);
+    const auto& expected = std::vector<std::string>{
+        "()()()", "()(())", "(())()", "(()())", "((()))"
+    };
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    const int n = 1;
+    const auto result = Solution{}.generateParenthesis(n);
+    const auto& expected = std::vector<std::string>{
+        "()"
+    };
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/22-generate-parentheses/README.md
@@ -0,0 +1,17 @@
+﻿[22. Generate Parentheses](https://leetcode.com/problems/generate-parentheses/)
+
+Given `n` pairs of parentheses, write a function to _generate all combinations of well-formed parentheses_.
+
+__Example 1:__
+
+    Input: n = 3
+    Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+__Example 2:__
+
+    Input: n = 1
+    Output: ["()"]
+
+__Constraints:__
+
+- $1 \leq$ n $\leq 8$
