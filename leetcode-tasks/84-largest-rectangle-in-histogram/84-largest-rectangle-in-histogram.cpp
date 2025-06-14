77bc20f754116b4d306cb29a8b3a316fdd34cda8
Xelerezex <nikitavalderman@gmail.com>
2025-06-14 13:22:45 +0300

    master: Added solved 84 problem.

---
+++ leetcode-tasks/84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,128 @@
+﻿#include <gtest/gtest.h>
+#include <stack>
+
+class Solution
+{
+public:
+
+    int largestRectangleArea(std::vector<int>& heights)
+    {
+        int largestArea = 0;
+
+        // stack <value, current_min>
+        struct MinNode
+        {
+            int index;
+            int value;
+        };
+        std::stack<MinNode, std::vector<MinNode>> heightsStack;
+
+        // Creating stack with non-decreasing order with border count 
+        for (int index = 0; index < heights.size(); ++index)
+        {
+            const auto currentHeight = heights[index];
+
+            if (heightsStack.empty() 
+                || currentHeight >= heightsStack.top().value
+            )
+            {
+                heightsStack.push(MinNode{index, currentHeight});
+                largestArea = std::max(largestArea, currentHeight);
+            }
+            else if (currentHeight < heightsStack.top().value)
+            {
+                int indexOnStack = 0;
+
+                while (!heightsStack.empty() 
+                    && currentHeight < heightsStack.top().value
+                )
+                {
+                    const auto valueOnStack = heightsStack.top().value;
+                    indexOnStack = heightsStack.top().index;
+                    const auto areaFromStack = valueOnStack * (index - indexOnStack); 
+                    largestArea = std::max(largestArea, areaFromStack);
+                    
+                    heightsStack.pop();
+                }                
+                
+                heightsStack.push(MinNode{indexOnStack, currentHeight});
+            }
+        }
+
+        // Count data from stack
+        int maxIndex = static_cast<int>(heights.size());
+        while (!heightsStack.empty())
+        {
+            const auto valueOnStack = heightsStack.top().value;
+            const int indexOnStack = heightsStack.top().index;
+            const auto areaFromStack = valueOnStack * (maxIndex - indexOnStack); 
+            largestArea = std::max(largestArea, areaFromStack);
+            
+            heightsStack.pop();
+        }
+
+        return largestArea;
+    }
+};
+
+TEST (SolutionTest, CornerCase1)
+{
+    std::vector<int> heights = { 1, 2, 3, 4, 5 };
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 9;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, CornerCase2)
+{
+    std::vector<int> heights = { 1, 2, 2};
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 4;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example1)
+{
+    std::vector<int> heights = { 2,1,5,6,2,3 };
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 10;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::vector<int> heights = { 2, 4  };
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 4;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::vector<int> heights = { 7,1,7,2,2,4  };
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 8;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example4)
+{
+    std::vector<int> heights = { 1,3,7 };
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 7;
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example5)
+{
+    std::vector<int> heights = { 2,1, 2, 1, 2, 1,  2 };
+    const auto result = Solution{}.largestRectangleArea(heights);
+    const auto& expected = 7;
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/84-largest-rectangle-in-histogram/README.md
@@ -0,0 +1,24 @@
+﻿[84. Largest Rectangle in Histogram](https://leetcode.com/problems/largest-rectangle-in-histogram/)
+
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+__Example 1:__
+
+![image](./../../images/84-largest-rectangle-in-histogram-1.jpg)
+
+    Input: heights = [2,1,5,6,2,3]
+    Output: 10
+    Explanation: The above is a histogram where width of each bar is 1.
+    The largest rectangle is shown in the red area, which has an area = 10 units.
+
+__Example 2:__
+
+![image](./../../images/84-largest-rectangle-in-histogram-2.jpg)
+
+    Input: heights = [2,4]
+    Output: 4
+
+__Constraints:__
+
+- $ 1 <=$ `heights.length` $<= 10^5$
+- $0 <=$ `heights[i]` $<= 10^4$
