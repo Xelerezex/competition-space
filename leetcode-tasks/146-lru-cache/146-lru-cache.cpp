908d5830b98743836b6edc64a6a5e6a3ba69b02a
Xelerezex <nikitavalderman@gmail.com>
2025-06-22 15:24:56 +0300

    master: Update 146 task with my solution.

---
+++ leetcode-tasks/146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,176 @@
+﻿#include <gtest/gtest.h>
+
+class LRUCache
+{
+
+public:
+
+    LRUCache(int capacity)
+        : m_capacity{ capacity }
+    {
+    }
+    
+    int get(int key)
+    {
+        const auto foundIter = m_hash.find(key);
+        if (std::cend(m_hash) == foundIter)
+        {
+            return -1;
+        }
+        writeAsLRU(foundIter->second);
+
+        return m_lastRecentlyUsedIter->value;
+    }
+    
+    void put(int key, int value)
+    {
+        const auto foundIter = m_hash.find(key);
+    
+        // Update data
+        if (std::cend(m_hash) != foundIter)
+        {
+            foundIter->second->value = value;
+            writeAsLRU(foundIter->second);
+            return;
+        }
+
+        // Or else insert 
+        if (m_capacity > m_data.size())
+        {
+            pushValueToFront(key, value);
+        }
+        else
+        {   
+            // Remove Least Recently Used
+            const auto keyToDelete = std::begin(m_data)->key;
+            m_hash.erase(keyToDelete);
+            m_data.pop_front();
+            
+            // Push to front
+            pushValueToFront(key, value);
+        }
+    }
+
+private:
+    struct Node
+    {
+        int key{ -1 };
+        int value{ -1 };
+    };
+
+    void pushValueToFront(int key, int value)
+    {
+        // push_front
+        const auto dataIter = m_data.insert(
+            std::end(m_data), 
+            Node{ key, value }
+        );
+        m_hash.emplace(key, dataIter);
+    }
+
+    void writeAsLRU(const std::list<Node>::iterator& iter)
+    {
+        m_lastRecentlyUsedIter = iter;
+
+        // Move LRU element to tail
+        m_data.splice(
+            std::end(m_data), 
+            m_data, 
+            m_lastRecentlyUsedIter
+        );
+    }
+
+private:
+
+    int m_capacity;
+
+    std::list<Node> m_data;
+    std::unordered_map<int, std::list<Node>::iterator> m_hash;
+    std::list<Node>::const_iterator m_lastRecentlyUsedIter;
+
+};
+
+TEST (SolutionTest, CornerCase1)
+{
+    LRUCache cache(3);
+    
+    cache.put(1, 1); // {{1, 1}}
+    cache.put(2, 2); // {{1, 1}, {2, 2}}
+    cache.put(3, 3); // {{1, 1}, {2, 2}, {3, 3}}
+
+    EXPECT_EQ(1, cache.get(1)); // {{2, 2}, {3, 3}, {1, 1}}
+    EXPECT_EQ(2, cache.get(2)); // {{3, 3}, {1, 1}, {2, 2}}
+    EXPECT_EQ(-1, cache.get(4));
+    
+    cache.put(4, 4); // { {1, 1}, {2, 2}, {4, 4}}
+
+    EXPECT_EQ(1, cache.get(1)); // {{2, 2}, {4, 4}, {1, 1}}
+    EXPECT_EQ(2, cache.get(2)); // {{4, 4}, {1, 1}, {2, 2}}
+    EXPECT_EQ(-1, cache.get(3));
+    EXPECT_EQ(4, cache.get(4)); // {{1, 1}, {2, 2}, {4, 4}}
+    EXPECT_EQ(2, cache.get(2)); // {{1, 1}, {4, 4}, {2, 2}}
+
+    cache.put(1, 8); // {{4, 4}, {2, 2}, {1, 8}}
+    cache.put(3, 7); // {{2, 2}, {1, 8}, {3, 7}}
+
+    EXPECT_EQ(8, cache.get(1)); // {{3, 7}, {2, 2}, {1, 8}}
+    EXPECT_EQ(2, cache.get(2)); // {{3, 7}, {1, 8}, {2, 2}}
+    EXPECT_EQ(7, cache.get(3)); // {{1, 8}, {2, 2}, {3, 7}}
+    EXPECT_EQ(-1, cache.get(4));
+    EXPECT_EQ(-1, cache.get(5));
+    EXPECT_EQ(2, cache.get(2)); // {{1, 8}, {3, 7}, {2, 2}}
+    EXPECT_EQ(7, cache.get(3)); // {{1, 8}, {2, 2}, {3, 7}}
+    EXPECT_EQ(-1, cache.get(4));
+
+    cache.put(1, 9); // {{2, 2}, {3, 7}, {1, 9}}
+    cache.put(6, 6); // {{3, 7}, {1, 9}, {6, 6}}
+
+    EXPECT_EQ(9, cache.get(1)); // {{3, 7}, {6, 6}, {1, 9}}
+    EXPECT_EQ(-1, cache.get(2));
+    EXPECT_EQ(7, cache.get(3)); // {{6, 6}, {1, 9}, {3, 7}}
+    EXPECT_EQ(-1, cache.get(4));
+    EXPECT_EQ(-1, cache.get(5));
+    EXPECT_EQ(6, cache.get(6)); // {{1, 9}, {3, 7}, {6, 6},}
+}
+
+TEST (SolutionTest, Example1)
+{
+    LRUCache cache(2);
+    
+    cache.put(1, 10);
+
+    EXPECT_EQ(10, cache.get(1));
+    
+    cache.put(2, 20);
+    cache.put(3, 30);
+
+    EXPECT_EQ(20, cache.get(2));
+    EXPECT_EQ(-1, cache.get(1));
+}
+
+TEST (SolutionTest, Example2)
+{
+    LRUCache cache(2);
+    
+    cache.put(1, 1);
+    cache.put(2, 2);
+
+    EXPECT_EQ(1, cache.get(1));
+    
+    cache.put(3, 3);
+
+    EXPECT_EQ(-1, cache.get(2));
+
+    cache.put(4, 4);
+
+    EXPECT_EQ(-1, cache.get(1));
+    EXPECT_EQ(3, cache.get(3));
+    EXPECT_EQ(4, cache.get(4));
+
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/146-lru-cache/README.md
@@ -0,0 +1,44 @@
+﻿[146. LRU Cache](https://leetcode.com/problems/lru-cache/)
+
+Design a data structure that follows the constraints of a [Least Recently Used (LRU) cache](https://en.wikipedia.org/wiki/Cache_replacement_policies#LRU).
+
+Implement the `LRUCache` class:
+
+- `LRUCache(int capacity)` Initialize the LRU cache with positive size `capacity`.
+- `int get(int key)` Return the value of the `key` if the key exists, otherwise return `-1`.
+- `void put(int key, int value)` Update the value of the `key` if the `key` exists. Otherwise, add the `key-value` pair to the cache. If the number of keys exceeds the `capacity` from this operation, evict the least recently used key.
+
+The functions `get` and `put` must each run in $O(1)$ average time complexity.
+
+__Example 1:__
+
+_Input_
+
+    ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+    [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+
+_Output_
+
+    [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+__Explanation__
+
+    LRUCache lRUCache = new LRUCache(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1);    // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2);    // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1);    // return -1 (not found)
+    lRUCache.get(3);    // return 3
+    lRUCache.get(4);    // return 4
+
+__Constraints:__
+
+- $1 \leq$ capacity $\leq 3000$
+- $0 \leq$ key $\leq 10^4$
+- $0 \leq$ value $\leq 10^5$
+- At most $2 * 10^5$ calls will be made to `get` and `put`.
+
+![image](./../../images/146-lru-cache.png)
