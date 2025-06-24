62c86e03d051bc3f762da8aed566114bcc45c2fe
Xelerezex <nikitavalderman@gmail.com>
2025-06-24 12:54:50 +0300

    master: Added solved 23 problem.

---
+++ leetcode-tasks/23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -0,0 +1,197 @@
+﻿#include <gtest/gtest.h>
+#include <limits>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0) , next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+
+    ListNode* mergeTwoLists(ListNode* pFirst, ListNode* pSecond)
+    {
+        if (nullptr == pFirst && nullptr == pSecond)
+        {
+            return nullptr;
+        }
+
+        int firstValue = (nullptr != pFirst) ? pFirst->val : std::numeric_limits<int>::max(); // 1000
+        int secondValue = (nullptr != pSecond) ? pSecond->val : std::numeric_limits<int>::max();
+
+        // Set first value
+        ListNode* pHead = (firstValue < secondValue)
+            ? pFirst
+            : pSecond;   
+        if (firstValue < secondValue)
+        {
+            pFirst = pFirst->next;
+        }
+        else
+        {
+            pSecond = pSecond->next;
+        }
+        ListNode* pCurrent = pHead;
+
+        while (nullptr != pFirst || nullptr != pSecond)
+        {
+            firstValue = (nullptr != pFirst) ? pFirst->val : std::numeric_limits<int>::max(); // 100'000
+            secondValue = (nullptr != pSecond) ? pSecond->val : std::numeric_limits<int>::max();
+
+            if (firstValue < secondValue)
+            {
+                pCurrent->next = pFirst;
+                pCurrent = pCurrent->next;
+
+                // Next iteration
+                pFirst = (nullptr != pFirst) ? pFirst->next : nullptr;
+            }
+            else
+            {
+                pCurrent->next = pSecond;
+                pCurrent = pCurrent->next;
+
+                // Next iteration
+                pSecond = (nullptr != pSecond) ? pSecond->next : nullptr;
+            }
+        }
+        return pHead;
+    }
+
+    ListNode* mergeKLists(std::vector<ListNode*>& lists)
+    {   
+        if (lists.empty())
+        {
+            return nullptr;
+        }
+        else if (lists.size() == 1)
+        {
+            return mergeTwoLists(lists.at(0), nullptr);
+        }
+
+        ListNode* pMerged = mergeTwoLists(lists.at(0), lists.at(1));
+
+        for (int index = 2; index < lists.size(); ++index)
+        {
+            ListNode* pFirst = pMerged;
+            ListNode* pSecond = lists.at(index);
+            pMerged = mergeTwoLists(pFirst, pSecond);
+        }
+
+        return pMerged;                                            
+    }
+};
+
+TEST (SolutionTest, Example1)
+{
+    auto node3_1 = std::make_unique<ListNode>(5, nullptr); 
+    auto node2_1 = std::make_unique<ListNode>(4, node3_1.get()); 
+    auto node1_1 = std::make_unique<ListNode>(1, node2_1.get());
+    
+    auto node3_2 = std::make_unique<ListNode>(4, nullptr); 
+    auto node2_2 = std::make_unique<ListNode>(3, node3_2.get()); 
+    auto node1_2 = std::make_unique<ListNode>(1, node2_2.get());
+
+    auto node2_3 = std::make_unique<ListNode>(6, nullptr); 
+    auto node1_3 = std::make_unique<ListNode>(2, node2_3.get());
+
+    std::vector<ListNode*> input{ node1_1.get(), node1_2.get(), node1_3.get() };
+
+    auto pResult = Solution{}.mergeKLists(input);
+    std::vector<int> result;
+    for (; nullptr != pResult; pResult = pResult->next)
+    {
+        result.push_back(pResult->val);
+    }
+
+    std::vector<int> expected{
+        1, 1, 2, 3, 4, 4, 5, 6
+    };
+
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example2)
+{
+    std::vector<ListNode*> input{};
+
+    auto pResult = Solution{}.mergeKLists(input);
+    std::vector<int> result;
+    std::vector<int> expected;
+
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example3)
+{
+    std::vector<ListNode*> input{ nullptr, nullptr };
+
+    auto pResult = Solution{}.mergeKLists(input);
+    std::vector<int> result;
+    std::vector<int> expected;
+
+    EXPECT_EQ(expected, result);
+}
+
+TEST (SolutionTest, Example4)
+{
+    auto node3_1 = std::make_unique<ListNode>(4, nullptr); 
+    auto node2_1 = std::make_unique<ListNode>(2, node3_1.get()); 
+    auto node1_1 = std::make_unique<ListNode>(1, node2_1.get());
+    
+    auto node3_2 = std::make_unique<ListNode>(5, nullptr); 
+    auto node2_2 = std::make_unique<ListNode>(3, node3_2.get()); 
+    auto node1_2 = std::make_unique<ListNode>(1, node2_2.get());
+
+    auto node2_3 = std::make_unique<ListNode>(6, nullptr); 
+    auto node1_3 = std::make_unique<ListNode>(3, node2_3.get());
+
+    std::vector<ListNode*> input{ node1_1.get(), node1_2.get(), node1_3.get() };
+
+    auto pResult = Solution{}.mergeKLists(input);
+    std::vector<int> result;
+    for (; nullptr != pResult; pResult = pResult->next)
+    {
+        result.push_back(pResult->val);
+    }
+
+    std::vector<int> expected{
+        1,1,2,3,3,4,5,6
+    };
+
+    EXPECT_EQ(expected, result);
+}
+
+
+TEST (SolutionTest, Example5)
+{
+    auto node3_1 = std::make_unique<ListNode>(99, nullptr); 
+    auto node2_1 = std::make_unique<ListNode>(88, node3_1.get()); 
+    auto node1_1 = std::make_unique<ListNode>(7, node2_1.get());
+
+    std::vector<ListNode*> input{ node1_1.get() };
+
+    auto pResult = Solution{}.mergeKLists(input);
+    std::vector<int> result;
+    for (; nullptr != pResult; pResult = pResult->next)
+    {
+        result.push_back(pResult->val);
+    }
+
+    std::vector<int> expected{
+        7, 88, 99
+    };
+
+    EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ leetcode-tasks/23-merge-k-sorted-lists/README.md
@@ -0,0 +1,37 @@
+﻿[23. Merge k Sorted Lists](https://leetcode.com/problems/merge-k-sorted-lists/)
+
+You are given an array of `k` linked-lists `lists`, each linked-list is sorted in ascending order.
+
+_Merge all the linked-lists into one sorted linked-list and return it._
+
+__Example 1:__
+
+    Input: lists = [[1,4,5],[1,3,4],[2,6]]
+    Output: [1,1,2,3,4,4,5,6]
+    Explanation: The linked-lists are:
+    [
+        1->4->5,
+        1->3->4,
+        2->6
+    ]
+    merging them into one sorted list:
+    1->1->2->3->4->4->5->6
+
+__Example 2:__
+
+    Input: lists = []
+    Output: []
+
+__Example 3:__
+
+    Input: lists = [[]]
+    Output: []
+
+__Constraints:__
+
+- `k == lists.length`
+- $0 \leq$ `k` $\leq 104$
+- $0 \leq$ lists[i].length $\leq 500$
+- $-10^4 \leq$ lists[i][j] $\leq 10^4$
+- `lists[i]` is sorted in __ascending order__.
+- The sum of `lists[i].length` will not exceed $10^4$.
